2d716d2ff521160bf321eb9b52fc7bf48c191bf2
dracmas <chris.palmer@gmx.com>
2014-12-14 01:03:31 +0000

    edited social channel

--- src/act.comm.c
+++ src/act.comm.c
@@ -50,7 +50,7 @@ ACMD(do_say)
     if (CONFIG_SPECIAL_IN_COMM && legal_communication(argument))
       parse_at(argument);
 
-    snprintf(buf, sizeof(buf), "$n\tn says, '%s'", argument);
+    snprintf(buf, sizeof(buf), "\x1B[1;32m$n says, '%s'\x1B[0;0m", argument);
     msg = act(buf, FALSE, ch, 0, 0, TO_ROOM | DG_NO_TRIG);
 
     for (vict = world[IN_ROOM(ch)].people; vict; vict = vict->next_in_room)
@@ -60,7 +60,7 @@ ACMD(do_say)
     if (!IS_NPC(ch) && PRF_FLAGGED(ch, PRF_NOREPEAT))
       send_to_char(ch, "%s", CONFIG_OK);
     else {
-      sprintf(buf, "You say, '%s'", argument);
+      sprintf(buf, "\x1B[1;32mYou say, '%s'\x1B[0;0m", argument);
       msg = act(buf, FALSE, ch, 0, 0, TO_CHAR | DG_NO_TRIG);
       add_history(ch, msg, HIST_SAY);
     }
